ba2c7f1e897ef0c5a2ccd2f2605a71d2b49cba39
Isaiah-Haynes <112427446+Isaiah-Haynes@users.noreply.github.com>
2024-03-27 12:11:49 -0400

    After Class Mar 27

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -7,8 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.criminalintent.databinding.FragmentCrimeListBinding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 private const val TAG = "CrimeListFragment"
 class CrimeListFragment : Fragment() {
@@ -21,10 +26,25 @@ class CrimeListFragment : Fragment() {
             "Binding is null. Can you see the view?"
         }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
-    }
+//    private var job: Job? = null
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
+//    }
+
+//    override fun onStart() {
+//        super.onStart()
+//        job = viewLifecycleOwner.lifecycleScope.launch {
+//            val crimes = crimeListViewModel.loadCrimes()
+//            binding.crimeRecyclerView.adapter = CrimeListAdapter(crimes)
+//        }
+//    }
+//
+//    override fun onStop() {
+//        super.onStop()
+//        job?.cancel()
+//    }
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -38,8 +58,21 @@ class CrimeListFragment : Fragment() {
     ): View? {
         _binding = FragmentCrimeListBinding.inflate(layoutInflater, container, false)
         binding.crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+//        val crimes = crimeListViewModel.crimes
+//        val adapter = CrimeListAdapter(crimes)
+//        binding.crimeRecyclerView.adapter = adapter
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
+                val crimes = crimeListViewModel.loadCrimes()
+                binding.crimeRecyclerView.adapter = CrimeListAdapter(crimes)
+            }
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/criminalintent/CrimeRepository.kt
@@ -0,0 +1,17 @@
+package com.example.criminalintent
+
+import android.content.Context
+
+class CrimeRepository private constructor(context: Context) {
+
+    companion object{
+        private var INSTANCE : CrimeRepository? = null
+
+        fun initialize(context : Context) {
+            if (INSTANCE == null) {
+                INSTANCE = CrimeRepository(context)
+            } // making a singleton
+        }
+    }
+
+}
