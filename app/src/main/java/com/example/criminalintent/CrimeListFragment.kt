1234ce936afce4f235f4829df933f8cff7239894
Isaiah-Haynes <112427446+Isaiah-Haynes@users.noreply.github.com>
2024-04-01 13:11:05 -0400

    updates after class April 3

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.criminalintent.databinding.FragmentCrimeListBinding
 import kotlinx.coroutines.launch
@@ -67,7 +68,11 @@ class CrimeListFragment : Fragment() {
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
                 crimeListViewModel.crimes.collect{ crimes->
-                    binding.crimeRecyclerView.adapter = CrimeListAdapter(crimes)
+                    binding.crimeRecyclerView.adapter = CrimeListAdapter(crimes){crimeID ->
+                        findNavController().navigate(
+                            /*R.id.show_crime_detail*/
+                        CrimeListFragmentDirections.showCrimeDetail(crimeID))
+                    }
                 }
 
             }
--- app/src/main/java/com/example/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/example/criminalintent/CrimeRepository.kt
@@ -3,12 +3,18 @@ package com.example.criminalintent
 import android.content.Context
 import androidx.room.Room
 import database.CrimeDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import java.util.UUID
 
 private const val DATABASE_NAME = "crime-database"
 
-class CrimeRepository private constructor(context: Context) {
+class CrimeRepository private constructor(
+    context: Context,
+    private val coroutineScope: CoroutineScope = GlobalScope
+) {
 
     private val database: CrimeDatabase = Room.databaseBuilder(
         context.applicationContext,
@@ -21,6 +27,13 @@ class CrimeRepository private constructor(context: Context) {
     /*suspend*/ fun getCrimes() : Flow<List<Crime>> = database.crimeDao().getCrimes()
 
     suspend fun getCrime(id:UUID) : Crime = database.crimeDao().getCrime(id)
+
+    /*suspend*/ fun updateCrime(crime: Crime){
+        coroutineScope.launch {
+            database.crimeDao().updateCrime(crime)
+        }
+
+    }
 
     companion object{
         private var INSTANCE : CrimeRepository? = null
